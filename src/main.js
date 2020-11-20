59fc908a1747318b055d1e3508cab2ed82c2a962
Zahnen Garner <zahnen@gmail.com>
2020-11-20 12:01:19 -0800

    add logic to correctly target conversion rates, convert single rate

--- src/main.js
+++ src/main.js
@@ -4,17 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from './js/exchange-service.js';
 
+// function convert() {
+//   let amount = $('#amount').val();
+//   let usdTo = $("select#convertTo").val();
+//   let final = (amount * response.conversion_rates.USD)
+//   $('#results').text(`$ ${amount} USD is equal to ${usdTo}. ${final}`);
+// }
+
 $(document).ready(function() {
   $('#exchangeForm').submit(function(event) {
     event.preventDefault();
     Exchange.convertUSD()
       .then(function(response) {
-        console.log(response);
-        console.log(response.conversion_rates);
+        let amount = parseInt($('#amount').val());
+        let usdTo = $("select#convertTo").val();
+        let rate = response.conversion_rates.EUR;
+        let final = (amount * rate);
+        $('#results').text(`$ ${amount} USD is equal to ${final} ${usdTo}.`);
       });
   });
 });
-
-// let amount = $('#amount').val();
-// let usdTo = $("select#convertTo").val();
-// $('#results').text(`$ ${amount} USD is equal to ${usdTo}.`);
