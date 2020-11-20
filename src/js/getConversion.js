122187512bfa47cb4410d51b34e0d273533fa0bf
Zahnen Garner <zahnen@gmail.com>
2020-11-20 15:32:59 -0800

    add ability for user to input currency code via string

--- src/js/getConversion.js
+++ src/js/getConversion.js
@@ -2,11 +2,10 @@ import $ from 'jquery';
 
 export default function getConversion(response) {
   let amount = parseInt($('#amount').val());
-  let usdTo = $("select#convertTo").val();
+  let usdTo = ($("#convertTo").val()).toUpperCase();
   if (response.result === "success" && typeof response.conversion_rates[`${usdTo}`] != "undefined") {
     let final = (amount * response.conversion_rates[`${usdTo}`]);
     $('#results').text(`$ ${amount} USD is equal to ${final} ${usdTo}`);
-    console.log(response.conversion_rates.xyz);
   } else if (response.result === "success" && typeof response.conversion_rates[`${usdTo}`] == "undefined") {
     $('#results').text(`We're sorry, but that doesn't look to be a currency our application supports`);
   } else {
