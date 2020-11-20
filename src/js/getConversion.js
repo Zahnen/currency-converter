663a2a6f5a8e4342177ad2a2873c01d06caa95e8
Zahnen Garner <zahnen@gmail.com>
2020-11-20 15:38:46 -0800

    add resource to error message for supported currency codes

--- src/js/getConversion.js
+++ src/js/getConversion.js
@@ -7,7 +7,7 @@ export default function getConversion(response) {
     let final = (amount * response.conversion_rates[`${usdTo}`]);
     $('#results').text(`$ ${amount} USD is equal to ${final} ${usdTo}`);
   } else if (response.result === "success" && typeof response.conversion_rates[`${usdTo}`] == "undefined") {
-    $('#results').text(`We're sorry, but that doesn't look to be a currency our application supports`);
+    $('#results').html(`We're sorry, but that doesn't look to be a currency our application supports. To view a full list of supported currencies, <a href="https://www.exchangerate-api.com/docs/supported-currencies" target="_blank"> click here.</a>`);
   } else {
     $('#results').text(`We're sorry, but an error has occured. Error: ${response[`error-type`]}`);
   }
